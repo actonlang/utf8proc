e914c63b43d5f283090a63a307fccd25acbe37f0
Kristian Larsson <kristian@spritelink.net>
2024-08-26 21:40:55 +0200

    Support static & shared lib build

--- build.zig
+++ build.zig
@@ -5,21 +5,32 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary(.{
+    const build_shared_libs = b.option(bool, "BUILD_SHARED_LIBS",
+                "Build shared libraries (otherwise static ones)") orelse true;
+
+    var lib = b.addStaticLibrary(.{
         .name = "utf8proc",
         .target = target,
         .optimize = optimize,
     });
+    if (build_shared_libs) {
+        lib = b.addSharedLibrary(.{
+            .name = "utf8proc",
+            .target = target,
+            .optimize = optimize,
+        });
+    }
 
-    const source_files = [_][]const u8{
-        "utf8proc.c",
-    };
+    var flags = std.ArrayList([]const u8).init(b.allocator);
+    defer flags.deinit();
 
-    lib.addCSourceFiles(.{
-        .files = &source_files,
-        .flags = &[_][]const u8{
-            "-DUTF8PROC_STATIC",
-        },
+    if (!build_shared_libs) {
+        flags.append("-DUTF8PROC_STATIC") catch unreachable;
+    }
+
+    lib.addCSourceFile(.{
+        .file = b.path("utf8proc.c"),
+        .flags = flags.items
     });
     lib.linkLibC();
     lib.installHeader(b.path("utf8proc.h"), "utf8proc.h");
